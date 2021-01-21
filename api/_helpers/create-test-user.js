8a46ac089459113c85f9763f8a14d368d9fb48b9
Matthias Weis <matthias1.weis@web.de>
2021-01-21 22:04:02 +0100

    use ENV variable for admin password in prod env

--- api/_helpers/create-test-user.js
+++ api/_helpers/create-test-user.js
@@ -7,19 +7,31 @@ module.exports = createTestUser;
 async function createTestUser() {
     // create test user if the db is empty
     if ((await db.User.countDocuments({})) === 0) {
-        var newUser = {
-            firstName: 'Test',
-            lastName: 'User',
-            username: 'testadmin',
-            passwordHash: bcrypt.hashSync('test', 10),
-            role: [Role.Admin]
+        if("ADMIN_PASSWORD" in process.env){
+            var newUser = {
+                firstName: 'Test',
+                lastName: 'User',
+                username: 'nobodyisadmin',
+                passwordHash: bcrypt.hashSync(process.env.ADMIN_PASSWORD, 10),
+                role: [Role.Admin]
+            }
+            await db.User(newUser).save();
+        }else{
+            // test environment
+            var newUser = {
+                firstName: 'Test',
+                lastName: 'User',
+                username: 'nobodyisadmin',
+                passwordHash: bcrypt.hashSync('test', 10),
+                role: [Role.Admin]
+            }
+            await db.User(newUser).save();
+            newUser.username = 'testuser';
+            newUser.role = [Role.User];
+            await db.User(newUser).save();
+            newUser.username = 'testgame';
+            newUser.role = [Role.GameMaster];
+            await db.User(newUser).save();
         }
-        await db.User(newUser).save();
-        newUser.username = 'testuser';
-        newUser.role = [Role.User];
-        await db.User(newUser).save();
-        newUser.username = 'testgame';
-        newUser.role = [Role.GameMaster];
-        await db.User(newUser).save();
     }
 }
